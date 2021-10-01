4153f2442a6016925296494ebd69f345b3d189be
kellyrgriff <kellyrgriff@hotmail.com>
2021-09-30 20:06:57 -0400

    editing date

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+function formatDate(timestamp)
+
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -5,11 +8,14 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = "friday 5:00";
 }
 
 let apiKey = "b8bc459fecfe469ecfc364c44094e49b";
